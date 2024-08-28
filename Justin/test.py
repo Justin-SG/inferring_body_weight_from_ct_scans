46b48b42a797dd9d4f8015468ce123983db8ede5
Justin-SG <schoenberg.justin@gmail.com>
2024-08-28 16:00:39 +0200

    Cleaning Repository

--- Justin/test.py
+++
@@ -1,7 +0,0 @@
-from totalsegmentator.python_api import totalsegmentator
-
-input_path = "C:/Users/schoe/Desktop/inferring_body_weight_from_ct_scans/0_Data_Understanding/CT_example/1001EE27"
-output_path = "test2"
-
-if __name__ == "__main__":
-    totalsegmentator(input_path, output_path, fast=True, preview=True)
