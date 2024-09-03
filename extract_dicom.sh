153c00540d1358f12b189e56ecf2b72b4c542beb
Florian Hinterberger <fhinterberger.its-m2023@gmail.com>
2024-09-03 16:10:28 +0200

    small change extract_dicom.sh

--- extract_dicom.sh
+++ extract_dicom.sh
@@ -8,19 +8,14 @@
 #SBATCH --time=1-00:00                                      # Runtime in D-HH:MM
 #SBATCH --mem-per-cpu=1000M                                 # Memory in MB per CPU allocated
 
-set -e  # Exit immediately if a command exits with a non-zero status
 
-# Output hostname for debugging purposes
-hostname
-
-# Activate conda environment and log any errors
-source /home2/jschoenberg/miniconda/bin/activate 2>&1 | tee -a env_activation.log
-conda activate /home2/jschoenberg/miniconda/envs/infer_body/ 2>&1 | tee -a env_activation.log
+eval "$(conda shell.bash hook)"
+conda activate /home2/jschoenberg/miniconda/envs/infer_body/
 
 # Navigate to the working directory
 cd /srv/GadM/Datasets/Tmp/inferring_body_weight_from_ct_scans/1_Data_Extraction
 
 # Run the Python script and log output
-python DICOM_to_Dataframe.py -r /srv/GadM/Datasets/AIBA_CT_KG 2>&1 | tee -a script_output.log
+python DICOM_to_Dataframe.py -r /srv/GadM/Datasets/AIBA_CT_KG/
 
 exit
