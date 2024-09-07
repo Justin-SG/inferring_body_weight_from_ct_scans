86e22e97229a8287b95480e8a4ee58afc8d18a26
Florian Hinterberger <fhinterberger.its-m2023@gmail.com>
2024-09-07 17:25:01 +0200

    training shell script change

--- train_2d_nn.sh
+++ train_2d_nn.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1                                          # Number of processes
 #SBATCH --time=4-00:00                                      # Runtime in D-HH:MM
 #SBATCH --mem-per-cpu=4G                                    # Memory per CPU allocated
-#SBATCH --cpus-per-task=12                                  # CPU cores requested per task
+#SBATCH --cpus-per-task=10                                  # CPU cores requested per task
 
 
 eval "$(conda shell.bash hook)"
