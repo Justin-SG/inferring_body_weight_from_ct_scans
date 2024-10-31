95d479c9abe6fe51ff6ceb78543763aa3730088e
Justin-SG <schoenberg.justin@gmail.com>
2024-10-31 17:26:40 +0100

    Added alternative DICOM extraction because I need it

---
+++ extract_dicom_without_images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH --job-name=extract_dicom                            # Job name
+#SBATCH --output=Log/%x_%j.out                                  # Output file (includes job name and ID)
+#SBATCH --error=Log/%x_%j.err                                   # Error file (includes job name and ID)
+#SBATCH --gres=gpu:1                                        # Number of GPUs
+#SBATCH --ntasks=1                                          # Number of processes
+#SBATCH --time=1-00:00                                      # Runtime in D-HH:MM
+#SBATCH --mem-per-cpu=4G                                    # Memory per CPU allocated
+#SBATCH --cpus-per-task=8                                  # CPU cores requested per task
+
+
+eval "$(conda shell.bash hook)"
+conda activate /home2/jschoenberg/miniconda/envs/infer_body/
+
+
+cd /srv/GadM/Datasets/Tmp/inferring_body_weight_from_ct_scans/1_Data_Extraction
+
+echo "Extracting DICOM files to Dataframe"
+python DICOM_to_Dataframe.py /srv/GadM/Datasets/AIBA_CT_KG/ &&
+
+exit
