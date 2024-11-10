50a0ef8a99f1bb4792e120353d0323c471896593
Patrick Hofmann <phofmann.its-m2023@fh-salzburg.ac.at>
2024-11-10 23:07:05 +0100

    first init 3d_CNN

---
+++ 4_Modelling/Patrick/CustomModels.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class ResNetRegression(nn.Module):
+    def __init__(self, pretrained_model, num_outputs=1):
+        super(ResNetRegression, self).__init__()
+        
+        # Keep all layers of the pretrained model
+        self.backbone = nn.Sequential(*list(pretrained_model.children())[:-1])
+        
+        # Add global average pooling (if not already included in your pretrained model)
+        self.global_pool = nn.AdaptiveAvgPool3d(1)
+        
+        # Add the regression head (a fully connected layer)
+        self.fc = nn.Linear(2048, num_outputs)  # 2048 is the output of the last conv layer
+        
+    def forward(self, x):
+        # Forward pass through the backbone
+        x = self.backbone(x)
+        
+        # Global average pooling to reduce the 3D output to a single vector
+        x = self.global_pool(x)
+        
+        # Flatten the pooled output
+        x = torch.flatten(x, 1)
+        
+        # Regression output
+        x = self.fc(x)
+        
--- 4_Modelling/Patrick/Transforms/Transforms.py
+++ 4_Modelling/Patrick/Transforms/Transforms.py
@@ -1,66 +1,25 @@
 import numpy as np
 from .CustomTransforms import *
 from torchvision.transforms import v2
+import torchio as tio
+
+HOUNSFIELD_AIR, HOUNSFIELD_BONE = -1000, 1900
 
 
-def axial_projection_imagenet_transforms():
-    return v2.Compose([
-        # change the data type to uint32 -> prevents overflow
-        v2.Lambda(lambda x: x.astype(np.float32)),
-        # sum up all the pixel values over an axis and project them onto a 2D plane (Axial Projection)
-        v2.Lambda(lambda x: x.sum(axis=0)),
-        # add color channel dimension
-        v2.Lambda(lambda x: x[:, :, np.newaxis]),
-        # repeat color channel 3 times (RGB)
-        v2.Lambda(lambda x: np.repeat(x, 3, axis=2)),
-        # To PIL Image (required for torchvision transforms)
-        v2.ToImage(),
-        # Scale images to 224x224
-        v2.Resize((224, 224), antialias=True),
-        # Normalize tensor values to range [0, 1] with lambda function
-        ScaleTo01(),
-        # Normalize with mean and std
-        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+def cnn_3d():
+    return tio.Compose([
+    # Convert the input to a tensor
+    v2.Lambda(lambda x: x[:, :, np.newaxis]),
+    # dimesion D x W x C x H -> C x W x H x D
+    tio.Lambda(lambda x: x.permute(2, 1, 3, 0)),
+    # Normalize the pixel values to the range [0, 1]
+    tio.RescaleIntensity((HOUNSFIELD_AIR, HOUNSFIELD_BONE), (0, 1)),
 
+    # Crop or Pad the input to the target size (600, 512, 512)
+    tio.CropOrPad((512, 512, 600)),
 
-def coronal_projection_imagenet_transforms():
-    return v2.Compose([
-        # change the data type to uint32 -> prevents overflow
-        v2.Lambda(lambda x: x.astype(np.float32)),
-        # sum up all the pixel values over an axis and project them onto a 2D plane (Coronal Projection)
-        v2.Lambda(lambda x: x.sum(axis=1)),
-        # add color channel dimension
-        v2.Lambda(lambda x: x[:, :, np.newaxis]),
-        # repeat color channel 3 times (RGB)
-        v2.Lambda(lambda x: np.repeat(x, 3, axis=2)),
-        # To PIL Image (required for torchvision transforms)
-        v2.ToImage(),
-        # Scale images to 224x224
-        v2.Resize((224, 224), antialias=True),
-        # Normalize tensor values to range [0, 1] with lambda function
-        ScaleTo01(),
-        # Normalize with mean and std
-        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-
-def sagittal_projection_imagenet_transforms():
-    return v2.Compose([
-        # change the data type to uint32 -> prevents overflow
-        v2.Lambda(lambda x: x.astype(np.float32)),
-        # sum up all the pixel values over an axis and project them onto a 2D plane (Sagittal Projection)
-        v2.Lambda(lambda x: x.sum(axis=2)),
-        # add color channel dimension
-        v2.Lambda(lambda x: x[:, :, np.newaxis]),
-        # repeat color channel 3 times (RGB)
-        v2.Lambda(lambda x: np.repeat(x, 3, axis=2)),
-        # To PIL Image (required for torchvision transforms)
-        v2.ToImage(),
-        # Scale images to 224x224
-        v2.Resize((224, 224), antialias=True),
-        # Normalize tensor values to range [0, 1] with lambda function
-        ScaleTo01(),
-        # Normalize with mean and std
-        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    # Scale images to 224x224
+    tio.Resize((224, 224, 224)),
+    # Normalize with mean and std
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
