d018e3d26a39472b430491ec59ed9816ca53f939
Patrick Hofmann <phofmann.its-m2023@fh-salzburg.ac.at>
2024-11-14 17:25:19 +0100

    added something

--- train_3d_nn.sh
+++ train_3d_nn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=train_2d_nn                            # Job name
-#SBATCH --output=Log/%x_%j.out                                  # Output file (includes job name and ID)
-#SBATCH --error=Log/%x_%j.err                                   # Error file (includes job name and ID)
+#SBATCH --job-name=train_3d_nn                              # Job name
+#SBATCH --output=Log/%x_%j.out                              # Output file (includes job name and ID)
+#SBATCH --error=Log/%x_%j.err                               # Error file (includes job name and ID)
 #SBATCH --gres=gpu:2                                        # Number of GPUs
 #SBATCH --ntasks=1                                          # Number of processes
 #SBATCH --time=5-00:00                                      # Runtime in D-HH:MM
 #SBATCH --mem-per-cpu=4G                                    # Memory per CPU allocated
-#SBATCH --cpus-per-task=8                                  # CPU cores requested per task
+#SBATCH --cpus-per-task=8                                   # CPU cores requested per task
 
 
 eval "$(conda shell.bash hook)"
